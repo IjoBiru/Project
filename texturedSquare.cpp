090e6de8f5f467c583590957bfd0402bf0559385
IjoBiru <147489164+IjoBiru@users.noreply.github.com>
2023-12-06 19:13:32 +0700

    Add files via upload

--- texturedSquare.cpp
+++ texturedSquare.cpp
@@ -1,45 +1,67 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 // texturedSquare.cpp
 //
-//
-//
 // Alya Hani Nurahma	  (5222600032)
 // Haniefam Muslima Yahya (5222600060) 
-//
-// 
 //
 // Texture Credits: See ExperimenterSource/Textures/TEXTURE_CREDITS.txt
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <cstdlib>
 #include <iostream>
+#include <cmath>
 #include <fstream>
 
 #include <GL/glew.h>
 #include <GL/freeglut.h> 
-#include <GL/glut.h>
+//#include <GL/glut.h>
 
 #include "getBMP.h"
 
+#define ONE_BY_ROOT_THREE 0.57735
 
+GLfloat angle = 0.0f;
+
+static int step = 0;
 
 // Globals.
 static float Xangle = 0.0, Yangle = 0.0, Zangle = 0.0; // Angles to rotate torpedo.
 static int isAnimate = 0; // In animation mode?
-static int animationPeriod = 1; // Speed of animation.
+static int animationPeriod = 200; // Speed of animation.
 static unsigned int texture[2]; // Array of texture ids.
-static unsigned char chessboard[64][64][4]; // Storage for chessboard image.
+
 static float angle = 0.0; // Angle to rotate textured square.
 static int id = 0; // Currently displayed texture id.
 
-void initRendering() {
+GLfloat mat_amb_diff[] = { 0.7f, 0.7f, 0.7f, 1.0f };  // Ambien dan Diffuse
+GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };  // Spekular
+GLfloat mat_shininess[] = { 50.0f };                    // Nilai shininess
+
+
+float Color = 1.0f;
+
+// Box vertex normal vectors = normalized unit vector pointing from origin to vertex.
+/*static float normals[] =
+{
+	ONE_BY_ROOT_THREE, -ONE_BY_ROOT_THREE, ONE_BY_ROOT_THREE,
+	ONE_BY_ROOT_THREE, ONE_BY_ROOT_THREE, ONE_BY_ROOT_THREE,
+	ONE_BY_ROOT_THREE, ONE_BY_ROOT_THREE, -ONE_BY_ROOT_THREE,
+	ONE_BY_ROOT_THREE, -ONE_BY_ROOT_THREE, -ONE_BY_ROOT_THREE,
+	-ONE_BY_ROOT_THREE, -ONE_BY_ROOT_THREE, ONE_BY_ROOT_THREE,
+	-ONE_BY_ROOT_THREE, ONE_BY_ROOT_THREE, ONE_BY_ROOT_THREE,
+	-ONE_BY_ROOT_THREE, ONE_BY_ROOT_THREE, -ONE_BY_ROOT_THREE,
+	-ONE_BY_ROOT_THREE, -ONE_BY_ROOT_THREE, -ONE_BY_ROOT_THREE
+};*/
+
+/*void initRendering() {
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_COLOR_MATERIAL);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_NORMALIZE);
 	glShadeModel(GL_SMOOTH);
-}
+}*/
+
 
 // Load external textures.
 void loadTextures()
@@ -66,55 +88,21 @@ void loadTextures()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 }
 
-// Create 64 x 64 RGBA image of a chessboard.
-void createChessboard(void)
-{
-	int i, j;
-	for (i = 0; i < 64; i++)
-		for (j = 0; j < 64; j++)
-			if ((((i / 8) % 2) && ((j / 8) % 2)) || (!((i / 8) % 2) && !((j / 8) % 2)))
-			{
-				chessboard[i][j][0] = 0x00;
-				chessboard[i][j][1] = 0x00;
-				chessboard[i][j][2] = 0x00;
-				chessboard[i][j][3] = 0xFF;
-			}
-			else
-			{
-				chessboard[i][j][0] = 0xDD;
-				chessboard[i][j][1] = 0xDD;
-				chessboard[i][j][2] = 0xDD;
-				chessboard[i][j][3] = 0xFF;
-			}
-}
-
-// Routine to load a program-generated image as a texture. 
-void loadChessboardTexture()
-{
-	// Generate internal texture.
-	createChessboard();
-
-	// Create texture object texture[1]. 
-	glBindTexture(GL_TEXTURE_2D, texture[1]);
-
-	// Specify image data for currently active texture object.
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 64, 64, 0, GL_RGBA, GL_UNSIGNED_BYTE, chessboard);
-
-	// Set texture parameters for wrapping.
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	// Set texture parameters for filtering.
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-}
 
 // Initialization routine.
 void setup(void)
 {
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glEnable(GL_DEPTH_TEST);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+
+	GLfloat light_position[] = { 0.0f, 0.0f, 1.0f, 0.0f }; // Cahaya datang dari arah z positif
+	GLfloat light_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f }; // Warna cahaya putih
+
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, mat_amb_diff);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 
 	// Create texture ids.
 	glGenTextures(2, texture);
@@ -122,103 +110,81 @@ void setup(void)
 	// Load external texture. 
 	loadTextures();
 
-	// Load internal texture.
-	loadChessboardTexture();
-
-	// Specify how texture values combine with current surface color values.
-	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-
-	
 	// Turn on OpenGL texturing.
 	glEnable(GL_TEXTURE_2D);
 
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glEnable(GL_DEPTH_TEST); // Enable depth testing.
 
-	//Turn on lighting
-	glEnable(GL_LIGHTING);
-
-	// Light property vectors.
-	float lightAmb[] = { 0.0, 0.0, 0.0, 1.0 };
-	float lightDifAndSpec[] = { 1.0, 1.0, 1.0, 1.0 };
-	float lightPos[] = { 0.0, 1.5, 3.0, 1.0 };
-	float globAmb[] = { 0.2, 0.2, 0.2, 1.0 };
-
-	// Light properties.
-	glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmb);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDifAndSpec);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, lightDifAndSpec);
-	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
-
-	glEnable(GL_LIGHT0); // Enable particular light source.
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globAmb); // Global ambient light.
-	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE); // Enable two-sided lighting.
-	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE); // Enable local viewpoint.
-
-	// Cull back faces.
-	glEnable(GL_CULL_FACE);
-	glCullFace(GL_BACK);
 }
 
 void drawBox(void)
 {
-	glColor3f(0.0f, 0.0f, 0.0f);
-	glPushMatrix();
-	glScalef(13.3f, 13.3f, 6.6f);  // Atur skala lemari meja (panjang, lebar, tinggi)
-	glTranslatef(0.0f, 0.0f, 0.0f);  // Geser ke bawah
-	glutSolidCube(1.5);  // Gambar kubus solid
-	glPopMatrix();
-
-	glEnable(GL_TEXTURE_2D);
-
-	glBindTexture(GL_TEXTURE_2D, texture[id]);
 	glBegin(GL_QUADS);
-	glNormal3f(0.0, 1.0, 0.0);
 
 	// Sisi depan
+	glNormal3f(0.0f, 0.0f, 1.0f); // Normal untuk sisi depan
 	glTexCoord2f(0.0, 0.0); glVertex3f(-10.0, -10.0, 5.0);   // Kiri Bawah
 	glTexCoord2f(1.0, 0.0); glVertex3f(10.0, -10.0, 5.0);    // Kanan Bawah
 	glTexCoord2f(1.0, 1.0); glVertex3f(10.0, 10.0, 5.0);     // Kanan Atas
 	glTexCoord2f(0.0, 1.0); glVertex3f(-10.0, 10.0, 5.0);    // Kiri Atas
 
-	glDisable(GL_TEXTURE_2D);
+	// Sisi belakang
+	glNormal3f(0.0f, 0.0f, -1.0f);
+	glTexCoord2f(0.0, 0.0); glVertex3f(-10.0, -10.0, -5.0);  // Kiri Bawah
+	glTexCoord2f(1.0, 0.0); glVertex3f(10.0, -10.0, -5.0);   // Kanan Bawah
+	glTexCoord2f(1.0, 1.0); glVertex3f(10.0, 10.0, -5.0);    // Kanan Atas
+	glTexCoord2f(0.0, 1.0); glVertex3f(-10.0, 10.0, -5.0);   // Kiri Atas
+
+	// Sisi kiri
+	glNormal3f(-1.0f, 0.0f, 0.0f);
+	glTexCoord2f(0.0, 0.0); glVertex3f(-10.0, -10.0, 5.0);   // Depan Bawah
+	glTexCoord2f(0.0, 0.0); glVertex3f(-10.0, -10.0, -5.0);  // Belakang Bawah
+	glTexCoord2f(0.0, 0.0); glVertex3f(-10.0, 10.0, -5.0);   // Belakang Atas
+	glTexCoord2f(0.0, 0.0); glVertex3f(-10.0, 10.0, 5.0);    // Depan Atas
+
+	// Sisi kanan
+	glNormal3f(1.0f, 0.0f, 1.0f);
+	glTexCoord2f(0.0, 0.0); glVertex3f(10.0, -10.0, 5.0);    // Depan Bawah
+	glTexCoord2f(0.0, 0.0); glVertex3f(10.0, -10.0, -5.0);   // Belakang Bawah
+	glTexCoord2f(0.0, 0.0); glVertex3f(10.0, 10.0, -5.0);    // Belakang Atas
+	glTexCoord2f(0.0, 0.0); glVertex3f(10.0, 10.0, 5.0);     // Depan Atas
+
+	// Sisi atas
+	glNormal3f(0.0f, 1.0f, 0.0f);
+	glTexCoord2f(0.0, 0.0); glVertex3f(-10.0, 10.0, 5.0);    // Kiri Depan
+	glTexCoord2f(0.0, 0.0); glVertex3f(10.0, 10.0, 5.0);     // Kanan Depan
+	glTexCoord2f(0.0, 0.0); glVertex3f(10.0, 10.0, -5.0);    // Kanan Belakang
+	glTexCoord2f(0.0, 0.0); glVertex3f(-10.0, 10.0, -5.0);   // Kiri Belakang
+
+	// Sisi bawah
+	glNormal3f(0.0f, -1.0f, 0.0f);
+	glTexCoord2f(0.0, 0.0); glVertex3f(-10.0, -10.0, 5.0);   // Kiri Depan
+	glTexCoord2f(0.0, 0.0); glVertex3f(10.0, -10.0, 5.0);    // Kanan Depan
+	glTexCoord2f(0.0, 0.0); glVertex3f(10.0, -10.0, -5.0);   // Kanan Belakang
+	glTexCoord2f(0.0, 0.0); glVertex3f(-10.0, -10.0, -5.0);  // Kiri Belakang
+
 
 	glEnd();
-	
+
 }
 
 // Drawing routine.
 void drawScene(void)
 {
-	
-	glEnable(GL_LIGHTING); // Aktifkan pencahayaan
-	glEnable(GL_LIGHT0); // Aktifkan sumber cahaya
-
-	// Setup sifat material
-	float lightPos[] = { 0.0, 2.0, 2.5, 1.0 };
-	float matAmbAndDif[] = { 0.0, 0.0, 0.0, 0.0 }; // Atur sifat ambien dan difus
-	float matSpec[] = { 10.0, 10.0, 10.0, 10.0 }; // Atur sifat specular
-	float matShine[] = { 100.0 }; // Atur shininess
-
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	gluLookAt(0.0, 0.0, 20.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	glRotatef(angle, 0.0, 1.0, 0.0);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_NORMALIZE); // Normalisasi vektor normal
+	glEnable(GL_DEPTH_TEST); 
 
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, matAmbAndDif);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, matSpec);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, matShine);
+	gluLookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	//glRotatef(angle, 0.0, 1.0, 0.0);
 
 	glRotatef(Zangle, 0.0, 0.0, 1.0);
 	glRotatef(Yangle, 0.0, 1.0, 0.0);
 	glRotatef(Xangle, 1.0, 0.0, 0.0);
-
-	glDisableClientState(GL_VERTEX_ARRAY);
-	glDisableClientState(GL_NORMAL_ARRAY);
-
-	glEnable(GL_TEXTURE_2D);
 
 	// Activate texture object.
 	glBindTexture(GL_TEXTURE_2D, texture[id]);
@@ -227,15 +193,13 @@ void drawScene(void)
 	drawBox();
 	glPopMatrix();
 
-	glDisable(GL_TEXTURE_2D);
-	glDisable(GL_LIGHTING);
 
 	glutSwapBuffers();
 }
 
 void animate(int value)
 {
-	if (isAnimate)
+	if (false)
 	{
 		angle += 10;
 		if (angle > 360.0) angle -= 360.0;
@@ -253,7 +217,9 @@ void resize(int w, int h)
 	glLoadIdentity();
 	glFrustum(-5.0, 5.0, -5.0, 5.0, 5.0, 100.0);
 	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+	//glLoadIdentity();
+
+
 }
 
 // Keyboard input processing routine.
@@ -304,14 +270,21 @@ void keyInput(unsigned char key, int x, int y)
 	}
 }
 
+void specialKeyInput(int key, int x, int y)
+{
+	if (key == GLUT_KEY_UP) if (step < 180) step++;;
+	if (key == GLUT_KEY_DOWN) if (step > 0) step--;;
+	glutPostRedisplay();
+}
 
 // Routine to output interaction instructions to the C++ window.
 void printInteraction(void)
 {
 	std::cout << "Interaction:" << std::endl;
 	std::cout << "Press the x, X, y, Y, z, Z keys to rotate the box." << std::endl
-		<< "Press space to toggle between textures." << std::endl
-		<< "Press delete to reset." << std::endl;
+		<< "Press space to toggle between textures." << std::endl;
+	std::cout << "Press up/down arrow keys to open/close the box." << std::endl;
+
 }
 
 // Main routine.
@@ -327,14 +300,16 @@ int main(int argc, char** argv)
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("box headset");
-	initRendering();
+	//initRendering();
 
+	glEnable(GL_DEPTH_TEST);
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(resize);
 	glutKeyboardFunc(keyInput);
+	glutSpecialFunc(specialKeyInput);
 
-	glewExperimental = GL_TRUE;
-	glewInit();
+	//glewExperimental = GL_TRUE;
+	//glewInit();
 
 	setup();
 
